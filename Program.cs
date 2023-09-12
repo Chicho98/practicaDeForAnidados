08986668d179e4f2bab28abaa96c3619b0fcf322
Chicho98 <leecordeiro99@gmail.com>
2023-09-12 18:38:10 -0300

    se agrega proyecto

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for anidados
+            Console.WriteLine("cuantas rows o filas?");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("cuantas columnas?");
+            int columnas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("que simbolo queres papu?");
+            string simbolo = Console.ReadLine(); 
+            
+             for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columnas; j++) 
+                {
+                    Console.Write(simbolo);
+                }
+                Console.WriteLine();
+            }
+             Console.ReadKey();
+        }
+    }
+}
